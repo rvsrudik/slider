da58aafedfbe1f2756c002c5e271f7c83623c43f
Vitalii <rvsrudik@gmail.com>
2017-11-03 15:25:10 +0200

    changed img array to slide object array

--- javascript/carausel.js
+++ javascript/carausel.js
@@ -1,8 +1,6 @@
 
-    var left_arrow = $('#left_arrow');
+var left_arrow = $('#left_arrow');
 var right_arrow = $('#right_arrow');
-var images_block = $('#images');
-
 
 var div_image_zero = $('#image_0');
 var div_image_left = $('#image_1');
@@ -52,32 +50,43 @@ function ft_fix_overflow(min, max, number) {
 function ft_createImageTag(src) {
     var img_elem = document.createElement('img');
     img_elem.src = src;
-    // $(img_elem).addClass("slider_images");
-
-    // console.log(imageTab[0]);
     return (img_elem);
 }
 
+function ft_create_slide_obj(src, title, text, stars, author) {
+    var obj = {};
 
-var imageTab = [];
+    obj.img = ft_createImageTag(src);
+    obj.title = title;
+    obj.text = text;
+    obj.stars = stars;
+    obj.author = author;
 
-imageTab.push(ft_createImageTag("img/0.jpg"));
-imageTab.push(ft_createImageTag("img/raz.png"));
-imageTab.push(ft_createImageTag("img/2.png"));
-imageTab.push(ft_createImageTag("img/3.png"));
-imageTab.push(ft_createImageTag("img/4.png"));
-imageTab.push(ft_createImageTag("img/5.png"));
-imageTab.push(ft_createImageTag("img/6.png"));
-imageTab.push(ft_createImageTag("img/7.png"));
-imageTab.push(ft_createImageTag("img/8.png"));
-imageTab.push(ft_createImageTag("img/9.png"));
+    return (obj);
+}
 
+
+
+
+var slidesTab = [];
+
+
+slidesTab.push(ft_create_slide_obj("img/0.jpg", "New Providence is the great UI kit", "Just then her head struck against the roof of the hall: in fact she was now more than nine feet high, and she at once took up the little golden key and hurried off to the garden door.", 2, "Cameron Dowman"));
+slidesTab.push(ft_create_slide_obj("img/raz.png", "Sebe derni", "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content.", 5, "Pes Sobaka"));
+slidesTab.push(ft_create_slide_obj("img/2.png", "The background-image", "Property sets one or more background images for an element By default, a background-image is placed at the top-left corner of an element, and repeated both vertically and horizontally.", 3, "Vitya Howard"));
+slidesTab.push(ft_create_slide_obj("img/3.png", "Designed and built" ,"With all the love in the world by @mdo and @fat. Maintained by the core team with the help of our contributors. Currently v4.0.0-alpha.6. Code licensed MIT, docs CC BY 3.0.", 1, "Divan Vovan"));
+slidesTab.push(ft_create_slide_obj("img/4.png", "New Providence is the great UI kit", "Just then her head struck against the roof of the hall: in fact she was now more than nine feet high, and she at once took up the little golden key and hurried off to the garden door.", 2, "Cameron Dowman"));
+slidesTab.push(ft_create_slide_obj("img/5.png", "Sebe derni", "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content.", 5, "Pes Sobaka"));
+slidesTab.push(ft_create_slide_obj("img/6.png", "The background-image", "Property sets one or more background images for an element By default, a background-image is placed at the top-left corner of an element, and repeated both vertically and horizontally.", 3, "Vitya Howard"));
+slidesTab.push(ft_create_slide_obj("img/7.png", "Designed and built" ,"With all the love in the world by @mdo and @fat. Maintained by the core team with the help of our contributors. Currently v4.0.0-alpha.6. Code licensed MIT, docs CC BY 3.0.", 1, "Divan Vovan"));
+slidesTab.push(ft_create_slide_obj("img/8.png", "Sebe derni", "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content.", 5, "Pes Sobaka"));
+slidesTab.push(ft_create_slide_obj("img/9.png", "The background-image", "Property sets one or more background images for an element By default, a background-image is placed at the top-left corner of an element, and repeated both vertically and horizontally.", 3, "Vitya Howard"));
 
 function ft_initial_slider()
 {
-    var left_img = imageTab[1];
-    var central_img = imageTab[0];
-    var right_img = imageTab[imageTab.length - 1];
+    var left_img = slidesTab[1].img;
+    var central_img = slidesTab[0].img;
+    var right_img = slidesTab[slidesTab.length - 1].img;
 
     $(left_img).attr('id', 'left_pic');
     $(central_img).attr('id', 'central_pic');
@@ -101,13 +110,9 @@ function ft_move_horisontal(element, distance_percent, speed)
 function ft_change_zero_image(zero_image_id, zero_img_div, image_tab) {
 
     var slides_count = image_tab.length;
-
     zero_image_id = ft_fix_overflow(0, slides_count - 1, zero_image_id);
-
-
     console.log("back image id = " + zero_image_id);
-
-    var new_zero_img = imageTab[zero_image_id];
+    var new_zero_img = slidesTab[zero_image_id].img;
     $(new_zero_img).attr('id', 'zero_pic');
 
     return (new_zero_img);
@@ -120,26 +125,15 @@ right_arrow.on("click", function () {
 
     ft_redeterm_elements();
 
-
     console.log("central id = " + central_image_id);
 
 
-    // console.log(center_img);
-
-    var tmp = ft_change_zero_image(central_image_id + 2,  div_image_zero, imageTab);
-    central_image_id = ft_fix_overflow(0, imageTab.length - 1, central_image_id + 1);
-    console.log("new central id = " + central_image_id);
-
-
-    // console.log(tmp);
-    // console.log(center_img);
-
-    console.log("\n\n");
+    var tmp = ft_change_zero_image(central_image_id + 2,  div_image_zero, slidesTab);
+    central_image_id = ft_fix_overflow(0, slidesTab.length - 1, central_image_id + 1);
 
     zero_img = tmp;
 
     div_image_zero.empty();
-
 
     ft_move_horisontal(div_image_zero, -33.333333, 500);
     ft_move_horisontal(div_image_left, 33.333333, 500);
@@ -171,36 +165,18 @@ right_arrow.on("click", function () {
     div_image_center.removeAttr("id").attr({"id" : "image_3"});
     center_img.removeAttr("id").attr({"id" : "right_pic"});
 
-
-    // console.log("Zero image after");
-    // console.log(zero_img);
-    // console.log("Left image after");
-    // console.log(left_img);
-    // console.log("Central image after");
-    // console.log(center_img);
-    // console.log("Right image after");
-    // console.log(right_img);
-    // console.log("\n");
 });
-
-
 
 
 left_arrow.on("click", function () {
 
     ft_redeterm_elements();
 
+    var tmp2 = ft_change_zero_image(central_image_id - 2, div_image_zero, slidesTab);
+    central_image_id = ft_fix_overflow(0, slidesTab.length - 1, central_image_id - 1);
 
-    console.log("old central id = " + central_image_id);
-
-    var tmp2 = ft_change_zero_image(central_image_id - 2, div_image_zero, imageTab);
-    central_image_id = ft_fix_overflow(0, imageTab.length - 1, central_image_id - 1);
-    console.log("new central id = " + central_image_id);
-
-    console.log("\n\n");
     zero_img = tmp2;
     div_image_zero.empty();
-
 
     ft_move_horisontal(div_image_zero, 33.333333, 500);
     ft_move_horisontal(div_image_left, 33.333333, 500);
@@ -208,7 +184,6 @@ left_arrow.on("click", function () {
     ft_move_horisontal(div_image_right, -33.333333, 500);
 
     div_image_zero.append(zero_img);
-
 
     right_img.animate({
         height: center_image_size,
@@ -232,16 +207,6 @@ left_arrow.on("click", function () {
     div_image_center.removeAttr("id").attr({"id" : "image_1"});
     center_img.removeAttr("id").attr({"id" : "left_pic"});
 
-
-    // console.log("Zero image after");
-    // console.log(zero_img);
-    // console.log("Left image after");
-    // console.log(left_img);
-    // console.log("Central image after");
-    // console.log(center_img);
-    // console.log("Right image after");
-    // console.log(right_img);
-    // console.log("\n");
 });
 
 
